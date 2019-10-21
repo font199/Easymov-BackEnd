50e05a1b8fca85a8b8c89557aec605c3ce5b21c5
Xavier <fontbx@gmail.com>
2019-10-21 23:05:50 +0200

    Servei per no crear usuaris duplicats

--- src/main/java/com/example/demo/service/UsuariService.java
+++ src/main/java/com/example/demo/service/UsuariService.java
@@ -1,8 +1,29 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Usuari;
+import com.example.demo.repository.UsuariRepo;
 
 @Service
 public class UsuariService {
 
+    @Autowired
+    private UsuariRepo usuariRepo;
+	
+	public Long registrar(Usuari usuari) {
+		
+		 Optional<Usuari> ussuariExistent =  usuariRepo.findByIdGoogle(usuari.getIdGoogle());
+		 
+	 	if(ussuariExistent.isPresent()) {
+    		return ussuariExistent.get().getId();
+    	} else {
+    		Usuari usuariCreat = usuariRepo.save(usuari);
+    		return usuariCreat.getId();
+    	}
+		
+	}
 }
