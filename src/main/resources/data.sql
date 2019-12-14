2c2e708cf62f416de2f6c8efe41651a34300c6ed
Xavier <fontbx@gmail.com>
2019-12-14 11:28:04 +0100

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,2 @@
 INSERT INTO USUARI (id,ID_GOOGLE, mail, nom, puntuacio)
 values (1, 'ssss', 'sss@sss.com', 'System', 0);
-
-INSERT INTO USUARI (id, ,ID_GOOGLE, mail, nom, puntuacio)
-values (100, 'a1111', 'ssdf@sdf.com', 'Pau', 10);
-
-INSERT INTO USUARI (id, ,ID_GOOGLE, mail, nom, puntuacio)
-values (101, 'a2222', 'ssd2f@sdf.com', 'Laia', 10);
-
-INSERT INTO USUARI (id, ,ID_GOOGLE, mail, nom, puntuacio)
-values (102, 'a3333', 'ssd3f@sdf.com', 'Toni', 0);
-
-INSERT INTO OBSTACLE(id, descripcio, foto_url, id_usuari_creador, latitud, longitud, nom)
-values (200, '22 escales grans', '2345345.com', 100, 41.932173, 2.241661, 'Vic Escales');
-
-INSERT INTO OBSTACLE(id, descripcio, foto_url, id_usuari_creador, latitud, longitud, nom)
-values (201, 'rampa amb pendent de 50 graus', '23453435.com', 101, 41.386873, 2.116137, 'Pendent pronunciada');
