5075b31ed1f39e6b767a41522f8a1bd8acb3150d
marcazu <95marc@gmail.com>
2020-01-04 20:17:11 +0100

    Usuari controller test casi acabat

--- src/test/java/com/example/demo/controllerTest/UsuariControllerTest.java
+++ src/test/java/com/example/demo/controllerTest/UsuariControllerTest.java
@@ -3,6 +3,8 @@ package com.example.demo.controllerTest;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,9 +22,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.dto.PuntuacioDto;
 import com.example.demo.dto.TokenResDto;
 import com.example.demo.dto.UsuariDto;
+import com.example.demo.dto.UsuariResDto;
 import com.example.demo.entity.Obstacle;
 import com.example.demo.entity.Usuari;
 import com.example.demo.rest.UsuariController;
@@ -42,6 +47,8 @@ public class UsuariControllerTest {
 	Obstacle obs;
 	UsuariDto uDto;
 	TokenResDto tokenResDto;
+	UsuariResDto usuariDto;
+	PuntuacioDto puntuacioDto;
 	
 	@Before
 	 public void beforeEachTest (){
@@ -62,10 +69,12 @@ public class UsuariControllerTest {
 		uDto.setPuntuacio(0);
 		
 		tokenResDto = mock(TokenResDto.class);
+		usuariDto = mock(UsuariResDto.class);
+		puntuacioDto.setPuntuacio(10);
 	}
 	
 	@Test
-	public void getconcreteUserTest() throws Exception {
+	public void registrarUsuariTest() throws Exception {
 		
 		Mockito.when(
 				usuariService.registrar(uDto)).thenReturn(tokenResDto);
@@ -79,5 +88,48 @@ public class UsuariControllerTest {
 		JSONAssert.assertEquals(expected,result.getResponse().getContentAsString(),false);
 		
 	}
+	
+	@Test
+	public void getUsuarisTest() throws Exception{
+		
+		Mockito.when(
+				usuariService.llistar()).thenReturn((List<UsuariDto>) tokenResDto);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+				"/rest/usuaris").accept(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+			
+	}
+	
+	@Test
+	public void getUsuariconcretTest() throws Exception{
+		
+		Mockito.when(
+				usuariService.buscar(Mockito.anyInt())).thenReturn((usuariDto));
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+				"/rest/usuari/{id}").accept(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String expected =  "{id:1,nom:test,fotoURL:,idGoogle:,mail:testmail,puntuacio:0}";
+		JSONAssert.assertEquals(expected,result.getResponse().getContentAsString(),false);
+
+	}
+	
+	@Test
+	public void puntuarUsuariconcretTest() throws Exception{
+		
+		Mockito.when(
+				usuariService.puntuar(uDto.getId(),puntuacioDto )).thenReturn(puntuacioDto.getPuntuacio());
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.put(
+				"/rest/puntuar/{id}").accept(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		JSONAssert.assertEquals("10",result.getResponse().getContentAsString(),false);
+	}
+	
+
 
 }
